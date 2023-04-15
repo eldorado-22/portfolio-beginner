d59a79042b9fbd8c500fbc11f45bd70de7574c89
eldorado-22 <jumashevv992@gmail.com>
2023-04-15 12:32:08 +0600

    portfolio

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer id="footer">
+            <div className="container">
+                <div className="footer">
+
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header id="header">
+            <div className="container">
+                <div className="header">
+
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
