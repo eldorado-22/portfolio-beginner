cd5e84eca66eedf2ba47a645f7029ad878bf01eb
Eldorado.JM <119120263+eldorado-22@users.noreply.github.com>
2023-04-15 13:02:09 +0600

    header (#1)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import logo from "./../../images/logo.svg"
 
 const Header = () => {
     return (
-        <header id="header">
+        <header id="header" className="bg-gray-900">
             <div className="container">
-                <div className="header">
+                <div className="header  flex items-center justify-between">
+                    <div className="header--nav flex items-center ">
+                        <img src={logo} alt="img"/>
+                        <h5 className="text text-2xl px-2 text-amber-50">Jumashev</h5>
+                    </div>
 
+                    <nav className="py-4 text-amber-50 ">
+                        <a href="#" className="">Home</a>
+                        <a href="#" className="px-5">About Me</a>
+                        <a href="#">Contact</a>
+                    </nav>
+                    <div className="">
+                        <button className="px-3 py-5 bg-transparent border-amber-500 text-amber-50">Dark</button>
+                        <button className="px-3 py-5 bg-transparent border-amber-500 text-amber-50">GitHub</button>
+                    </div>
                 </div>
             </div>
         </header>
--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -1,10 +1,27 @@
 import React from 'react';
-
+import heroImg from "./../../images/heroImg.png"
 const Hero = () => {
     return (
-        <div>
-            
-        </div>
+        <section id="hero" style={{
+            background: "rgba(1,1,21,0.91)"
+        }} className="">
+            <div className="cotainer">
+                <div className="hero py-10 flex items-center justify-around">
+                    <div className="hero--text">
+                        <h4 className="text-amber-50 uppercase text-2xl">Hello</h4>
+                        <h1 className="text-amber-50 uppercase text-3xl py-2">I'm Jumashev <br/> Eldorado</h1>
+                        <p className="text-gray-500 text-xl">I've been doing web design, front-end development for <br/>
+                            a year now. Do you need a website design, site layout, <br/>
+                            or maybe a turnkey website? Then contact me
+                        </p>
+                        <button className="text-xl px-10 mt-3 py-2 bg-black text-amber-50 rounded-3xl">Call Me</button>
+                    </div>
+                    <div className="hero--images">
+                        <img src={heroImg} alt="img"/>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
