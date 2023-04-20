acfa5703efaa256362a3c6066395f997e929d002
Eldorado.JM <119120263+eldorado-22@users.noreply.github.com>
2023-04-20 16:43:30 +0600

    about (#4)

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/hero";
 import Category from "./components/Category/category";
+import {useState} from "react";
 
 function App() {
+    const [dark, setDark] = useState(false)
+
     return (
-        <>
-            <Header/>
-            <Hero/>
-            <Category/>
+        <div style={{
+            background: dark ? "rgba(9,7,72,0.91)" : "black",
+        }}>
+            <Header setDark={setDark} dark={dark}/>
+            <Hero setDark={setDark} dark={dark}/>
+            <Category setDark={setDark} dark={dark}/>
             <Footer/>
-        </>
+        </div>
     );
 }
 
