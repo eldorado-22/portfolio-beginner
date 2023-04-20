a68110be347ad859c2a768163dd4b5250b0f7a72
eldorado-22 <jumashevv992@gmail.com>
2023-04-20 11:25:09 +0600

    category

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.scss';
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/hero";
+import Category from "./components/Category/category";
 
 function App() {
     return (
         <>
             <Header/>
             <Hero/>
+            <Category/>
             <Footer/>
         </>
     );
